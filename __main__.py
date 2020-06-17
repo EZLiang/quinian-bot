ed4f382fc4c6a0a5a10b074382a4fa216d76e474
EZLiang <47573436+EZLiang@users.noreply.github.com>
2020-06-17 18:07:12 -0400

    v1.2: Please fix help reaction

--- __main__.py
+++ __main__.py
@@ -1,8 +1,7 @@
 import discord, sys, random
 
 
-client = discord.Client(status="Ping me for a quine")
-user = client.user
+client = discord.Client()
 
 
 quined = []
@@ -25,18 +24,44 @@ quines = [
 ]
 
 
-def get_channel(name):
-    for channel in client.get_all_channels():
+watching = []
+watchingm = []
+
+
+def get_channel(name, guild):
+    for channel in guild:
         if channel.name == name:
             return channel
-    return None
+
+
+def get_emoji(name, guild):
+    for emoji in guild.emojis:
+        if emoji.name == name:
+            return emoji
+
+
+async def give_help(channel):
+    sent = await channel.send("I am a bot that makes self-reference. Just ping me with some text. Or, you might like "
+                              "some random stuff. I can:\n > :one: Get random quined phrases: `random quined`\n > "
+                              ":two: Post random quines: `random quine`\n > :three: Roll dice: `random diceroll`\nI "
+                              "am also very good at annoying people.")
 
 
 @client.event
 async def on_message(msg):
+    todel = []
+    for i in range(len(watching)):
+        watching[i][1] = watching[i][1] + 1
+        if watching[i][1] == 21:
+            todel.append(i - len(todel))
+    for i in todel:
+        watching.pop(i)
+        watchingm.pop(i)
     mention = client.user.mention.replace("@", "@!")
     if mention in msg.content:
         m = msg.content.split(mention + " ")[-1]
+        if m.lower().startswith("help"):
+            await give_help(msg.channel)
         if m.lower().startswith("what are you "):
             q = m.lower()[13:].split("?")[0]
             await msg.channel.send("What am I " + q + "? What are _you_ " + q + "?!?")
@@ -65,6 +90,44 @@ async def on_message(msg):
                                                                                          "quotation "
         await msg.channel.send(quine)
         quined.append(quine)
+        if len(quined) == 51:
+            quined.pop(0)
+
+    if client.user.mention in msg.content:
+        m = msg.content.split(client.user.mention + " ")[-1]
+        if m.lower().startswith("help"):
+            await give_help(msg.channel)
+        if m.lower().startswith("what are you "):
+            q = m.lower()[13:].split("?")[0]
+            await msg.channel.send("What am I " + q + "? What are _you_ " + q + "?!?")
+            return
+        if m[:7] == "random ":
+            query = m[7:]
+            if query.startswith("quined"):
+                if len(quined) == 0:
+                    await msg.channel.send(":slight_frown: We don't have any quined phrases now. Try again a little "
+                                           "later!")
+                    return
+                await msg.channel.send(random.choice(quined))
+                return
+            elif query.startswith("quine"):
+                quine = random.choice(quines)
+                await msg.channel.send("```" + quine[1] + "\n" + quine[0] + "\n```")
+            elif query.startswith("diceroll"):
+                await msg.channel.send(":game_die: :" + random.choice(["one", "two", "three", "four", "five", "six"]) +
+                                       ":")
+            else:
+                await msg.channel.send("Hey! I don't know how to do that!")
+                return
+            await msg.channel.send("Happy? :smirk:")
+            return
+        quine = "\"" + m[0].upper() + m[1:] + " when preceded by its quotation\" " + m + " when preceded by its " \
+                                                                                         "quotation "
+        await msg.channel.send(quine)
+        quined.append(quine)
+        if len(quined) == 51:
+            quined.pop(0)
+
     if "wtf" in msg.content or "fuck" in msg.content or "hell" in msg.content or "damn" in msg.content or "shut up" in\
             msg.content:
         await msg.channel.send("Watch your language " + msg.author.mention + ". If I had mod permission I would ban "
@@ -73,8 +136,26 @@ async def on_message(msg):
 
 
 @client.event
+async def on_reaction_add(re, user):
+    print(str(re.emoji))
+    if re.me:
+        return
+
+
+@client.event
 async def on_ready():
+    print(client.user.mention)
+    game = discord.Game("print out this program")
+    await client.change_presence(status="Ping me for a quine", activity=game)
     print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_member_join(mem):
+    channel = get_channel("general", mem.guild)
+    await channel.send("Welcome to " + mem.guild.name + ", " + mem.mention + "! :partying_face: I am a bot to help "
+                                                                             "make self reference (as if that were a "
+                                                                             "real thing people need bots for)")
 
 
 def main():
